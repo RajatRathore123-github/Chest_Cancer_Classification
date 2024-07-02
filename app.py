f2a14a593e43751e05a6c2c4d9788bcc3738f79a
Rajat Rathore <rajat12350iam@gmail.com>
2024-07-02 14:20:42 +0530

    feat: integrated user app with the model

--- app.py
+++ app.py
@@ -18,7 +18,31 @@ class ClientApp:
         self.classifier = PredictionPipeline(self.filename)
 
 
-@app.route("/", method=["GET"])
+@app.route("/", methods=["GET"])
 @cross_origin()
 def home():
-    return render_template("index.html")        
+    return render_template("index.html")
+
+
+
+@app.route("/train", methods=["GET", "POST"])
+@cross_origin()
+def trainRoute():
+    os.system("python main.py")
+    # os.system("dvc repro")
+    return "Training done successfully!"
+
+
+@app.route("/predict", methods=["POST"])
+@cross_origin()
+def predictRoute():
+    image = request.json["image"]
+    decodeImage(image, clApp.filename)
+    result = clApp.classiifier.predict()
+    return jsonify(result)
+
+
+
+if __name__ == "__main__":
+    clApp = ClientApp()
+    app.run(host="0.0.0.0", port=8080)
